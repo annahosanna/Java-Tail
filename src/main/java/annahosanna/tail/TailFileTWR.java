8c135da9a4cda446507623c543f26dd10fd784d0
annahosanna <annahosanna@gmail.com>
2024-10-17 09:51:59 -0400

    Reformat

--- src/main/java/annahosanna/tail/TailFileTWR.java
+++ src/main/java/annahosanna/tail/TailFileTWR.java
@@ -1,15 +1,16 @@
 package example;
- 
+
 import java.io.*;
-import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 class ReadFile {
-//     inPipe = new DataInputStream(new PipedInputStream(pipedOutputStream));
-//    outPipe = new DataOutputStream(new PipedOutputStream(pipedInputStream));
 
-  public static void main (String args []) {
+  //     inPipe = new DataInputStream(new PipedInputStream(pipedOutputStream));
+  //    outPipe = new DataOutputStream(new PipedOutputStream(pipedInputStream));
+
+  public static void main(String args[]) {
     // Send log messages to stout
     BasicConfigurator.configure();
     String filename = args[0];
@@ -22,13 +23,18 @@ class ReadFile {
         keepChecking = false;
         logger.error("The file might have been renamed or truncated!");
       } else if (fileLength > filePointer) {
-        try (RandomAccessFile raf = new RandomAccessFile(file.getCanonicalPath(), "r");) {
+        try (
+          RandomAccessFile raf = new RandomAccessFile(
+            file.getCanonicalPath(),
+            "r"
+          );
+        ) {
           raf.seek(filePointer);
           try (FileInputStream fis = new FileInputStream(raf.getFD());) {
             try (
               InputStreamReader isrf = new InputStreamReader(fis);
-              BufferedReader in = new BufferedReader(isrf);) {              
-
+              BufferedReader in = new BufferedReader(isrf);
+            ) {
               // Do something usefull
 
               filePointer = raf.getFilePointer();
@@ -37,12 +43,14 @@ class ReadFile {
               g.printStackTrace();
             }
           } catch (Exception f) {
-            logger.error("Could not create FileInputStream (and therefore also BufferedReader)");
+            logger.error(
+              "Could not create FileInputStream (and therefore also BufferedReader)"
+            );
             f.printStackTrace();
-          } 
+          }
         } catch (Exception h) {
           logger.error("Could not create RandomAccessFile");
-        } 
+        }
       }
     }
   }
